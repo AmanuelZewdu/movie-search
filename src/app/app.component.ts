0b32c7080b189c62c9cd1206d3e1f0fede324bb7
Amanuel Z <azewdu@excellerentsolutions.com>
2023-11-16 13:40:59 +0300

    movie search app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'movie-search';
+  searchTerm: string = '';
+  searchResults: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  searchMovies = () => {
+    const url = `https://www.omdbapi.com/?s=${this.searchTerm}&apiKey=e6feef72`;
+
+    this.http.get(url).subscribe((data: any) => {
+      this.searchResults = data.Search.slice(0, 3);
+      console.log('*********Movie*******');
+      console.log(this.searchResults);
+    });
+  };
+
+  redirectMovieURL = (imdbID: string) => {
+    const movieURL = `https://www.imdb.com/title/${imdbID}`;
+    window.open(movieURL, '_blank');
+  };
 }
